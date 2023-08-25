a27463f6b6294037a479308b6e8f02e7b1c1b025
cyber-meow <sjungle305@gmail.com>
2023-08-25 10:30:45 +0800

    load image features before saving

--- lycoris_eval/encoding/encode_image_features.py
+++ lycoris_eval/encoding/encode_image_features.py
@@ -126,6 +126,8 @@ def main(args):
                                                        args.batch_size,
                                                        (not args.no_autocast),
                                                        device=device)
+                npz = np.load(save_path)
+                image_features_all = dict(npz.items())
                 image_features_all[feature_key] = image_features.numpy()
 
                 # Ensure the directory exists
