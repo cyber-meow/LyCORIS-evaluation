185529d2037fc7f2d75c4166ffa231756fd813b4
cyber-meow <sjungle305@gmail.com>
2023-09-15 15:25:39 +0800

    add reweight metriccs

--- lycoris_eval/evaluation/eval_main.sh
+++ lycoris_eval/evaluation/eval_main.sh
@@ -2,14 +2,14 @@
 
 # Define common variables
 REF_DIR="/media/bangbang/howdy/lycoris_paper/lyco-finetune-local/ref_features"
-EVAL_DIR="/media/bangbang/howdy/lycoris_paper/lyco-finetune-local/main/generated_features/captions"
-METRIC_CSV="../../results/metrics/captions_metrics.csv"
+EVAL_DIR="/media/bangbang/howdy/lycoris_paper/lyco-finetune-local/main/generated_features/reweight/"
+METRIC_CSV="../../results/metrics/reweight_metrics.csv"
 
 # Run each python script with the appropriate parameters
-python eval_image_similarity.py --ref_dir $REF_DIR --eval_dir $EVAL_DIR --encoder_name clip-L-14 --metric_csv $METRIC_CSV
-python eval_image_similarity.py --ref_dir $REF_DIR --eval_dir $EVAL_DIR --encoder_name convnextv2-l --metric_csv $METRIC_CSV
-python eval_image_similarity.py --ref_dir $REF_DIR --eval_dir $EVAL_DIR/nt_correct --encoder_name dinov2-l-fb --metric_csv $METRIC_CSV
-python eval_image_diversity.py --eval_dir $EVAL_DIR --encoder_name dinov2-l-fb --metric_csv $METRIC_CSV
-python eval_image_diversity.py --eval_dir $EVAL_DIR --encoder_name clip-L-14 --metric_csv $METRIC_CSV
-python eval_image_diversity.py --eval_dir $EVAL_DIR --encoder_name convnextv2-l --metric_csv $METRIC_CSV
-python eval_text_similarity.py --ref_dir $REF_DIR --eval_dir $EVAL_DIR --metric_csv $METRIC_CSV
+python eval_image_similarity.py --ref_dir $REF_DIR --eval_dir $EVAL_DIR --encoder_name clip-L-14 --metric_csv $METRIC_CSV --extra_level 2
+python eval_image_similarity.py --ref_dir $REF_DIR --eval_dir $EVAL_DIR --encoder_name convnextv2-l --metric_csv $METRIC_CSV --extra_level 2
+python eval_image_similarity.py --ref_dir $REF_DIR --eval_dir $EVAL_DIR/nt_correct --encoder_name dinov2-l-fb --metric_csv $METRIC_CSV --extra_level 2
+python eval_image_diversity.py --eval_dir $EVAL_DIR --encoder_name dinov2-l-fb --metric_csv $METRIC_CSV --extra_level 2
+python eval_image_diversity.py --eval_dir $EVAL_DIR --encoder_name clip-L-14 --metric_csv $METRIC_CSV --extra_level 2
+python eval_image_diversity.py --eval_dir $EVAL_DIR --encoder_name convnextv2-l --metric_csv $METRIC_CSV --extra_level 2
+python eval_text_similarity.py --ref_dir $REF_DIR --eval_dir $EVAL_DIR --metric_csv $METRIC_CSV --extra_level 2
