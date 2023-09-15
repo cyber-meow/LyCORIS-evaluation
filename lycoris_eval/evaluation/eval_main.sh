1691246c9d1e94caaa601f0d95b0d83df8477e45
cyber-meow <sjungle305@gmail.com>
2023-09-15 19:42:43 +0800

    update metrics

--- lycoris_eval/evaluation/eval_main.sh
+++ lycoris_eval/evaluation/eval_main.sh
@@ -8,7 +8,7 @@ METRIC_CSV="../../results/metrics/reweight_metrics.csv"
 # Run each python script with the appropriate parameters
 python eval_image_similarity.py --ref_dir $REF_DIR --eval_dir $EVAL_DIR --encoder_name clip-L-14 --metric_csv $METRIC_CSV --extra_level 2
 python eval_image_similarity.py --ref_dir $REF_DIR --eval_dir $EVAL_DIR --encoder_name convnextv2-l --metric_csv $METRIC_CSV --extra_level 2
-python eval_image_similarity.py --ref_dir $REF_DIR --eval_dir $EVAL_DIR/nt_correct --encoder_name dinov2-l-fb --metric_csv $METRIC_CSV --extra_level 2
+python eval_image_similarity.py --ref_dir $REF_DIR --eval_dir $EVAL_DIR --encoder_name dinov2-l-fb --metric_csv $METRIC_CSV --extra_level 2
 python eval_image_diversity.py --eval_dir $EVAL_DIR --encoder_name dinov2-l-fb --metric_csv $METRIC_CSV --extra_level 2
 python eval_image_diversity.py --eval_dir $EVAL_DIR --encoder_name clip-L-14 --metric_csv $METRIC_CSV --extra_level 2
 python eval_image_diversity.py --eval_dir $EVAL_DIR --encoder_name convnextv2-l --metric_csv $METRIC_CSV --extra_level 2
