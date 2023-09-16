35806b20d6ed5adfcda697420c1b31e9f3bbf165
cyber-meow <sjungle305@gmail.com>
2023-09-16 19:39:32 +0200

    add training prompts

--- lycoris_eval/data_preparation/remove_images.py
+++ lycoris_eval/data_preparation/remove_images.py
@@ -2,11 +2,14 @@ import os
 import argparse
 
 
-def remove_files(directory, img_exts=['.jpg', '.png', '.jpeg', '.webp', '.gif']):
+def remove_files(directory,
+                 img_exts=['.jpg', '.png', '.jpeg', '.webp', '.gif']):
     for root, dirs, files in os.walk(directory):
         for filename in files:
             # Check if the file is an image and has a corresponding .txt
-            if any(filename.endswith(ext) for ext in img_exts) and os.path.exists(os.path.join(root, filename.rsplit('.', 1)[0] + '.txt')):
+            if (any(filename.endswith(ext) for ext in img_exts)
+                    and os.path.exists(
+                    os.path.join(root, filename.rsplit('.', 1)[0] + '.txt'))):
                 img_path = os.path.join(root, filename)
                 txt_path = os.path.join(
                     root, filename.rsplit('.', 1)[0] + '.txt')
